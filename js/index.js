ab0b339ff96d79ef76fca0a1f53fa75c2632e082
Thiago Arruda <thiago.arruda.09@hotmail.com>
2025-04-16 22:16:16 -0300

    cria a estrutura principal da aplicação e o front

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const SELECT_PRODUTOS = document.getElementById("Select_produtos");
+const INPUT_QUANTIDADE = document.getElementById("Input_quantidade");
+const INPUT_PRECO = document.getElementById("Input_preco");
+const TABLE_VENDAS = document.getElementById("tbody_vendas");
+
+function ListarCaixa(){
+    const url = "http://localhost:3000/caixa"
+}
+
+function AddVenda(){
+ 
+    
+   
+
+}
+
+
+
