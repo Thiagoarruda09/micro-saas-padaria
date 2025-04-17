4760dd0c172d58cfe174c0ee42bf02fa394279fa
Thiago Arruda <thiago.arruda.09@hotmail.com>
2025-04-17 09:57:35 -0300

    adiciona o armazenamento no localstorage e a função de limpar

--- js/index.js
+++ js/index.js
@@ -1,18 +1,70 @@
+
+
+
+
 const SELECT_PRODUTOS = document.getElementById("Select_produtos");
 const INPUT_QUANTIDADE = document.getElementById("Input_quantidade");
 const INPUT_PRECO = document.getElementById("Input_preco");
 const TABLE_VENDAS = document.getElementById("tbody_vendas");
+const CAIXA_COUNT = document.getElementById("Caixa_caunt");
 
-function ListarCaixa(){
-    const url = "http://localhost:3000/caixa"
+
+
+function adicionarLinhaNaTabela(venda) {
+    const novaLinha = document.createElement("tr");
+
+    novaLinha.innerHTML = `
+        <td>${venda.produto}</td>
+        <td>${venda.quantidade}</td>
+        <td>${venda.preco}</td>
+    `;
+
+    TABLE_VENDAS.appendChild(novaLinha);
 }
 
-function AddVenda(){
- 
+
+function AddVenda(event){
+    event.preventDefault();
+
+    const produto = SELECT_PRODUTOS.value
+    const quantidade = parseInt(INPUT_QUANTIDADE.value)
+    const preco = parseFloat(INPUT_PRECO.value)
+
+
+    const novaVenda = {
+        produto: produto,
+        quantidade: quantidade,
+        preco: preco
+    }
+    let vendas = JSON.parse(localStorage.getItem("vendas")) || [];
+
+    vendas.push(novaVenda);
+
+    localStorage.setItem("vendas", JSON.stringify(vendas));
+
+    adicionarLinhaNaTabela(novaVenda);
+    SELECT_PRODUTOS.value = "";
+    INPUT_QUANTIDADE.value = "";
+    INPUT_PRECO.value = "";
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const vendas = JSON.parse(localStorage.getItem("vendas")) || [];
+    vendas.forEach(venda => adicionarLinhaNaTabela(venda));
+});
+
+
+function LimparCaixa(){
     
-   
+
+    if(!confirm("Tem certeza que deseja limpar o caixa?")) {
+        return;
+    }else{
+        localStorage.removeItem("vendas");
+        TABLE_VENDAS.innerHTML = "";
+    }
+
 
 }
-
 
 
